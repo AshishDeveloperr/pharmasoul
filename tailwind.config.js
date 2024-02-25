eee8e02eb096fe36ea2a5e0701aec52cc9d31104
akcode7 <ashishgotapogo10@gmail.com>
2024-02-25 19:10:19 +0530

    tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{php,html,js}",
+  "./*.{html,php}",
+  "./src/css/**/*.css",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
